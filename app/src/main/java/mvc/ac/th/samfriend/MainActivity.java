ec11279fa49c2a59b15b1f08c35d44af64e7a627
krusammvc <krisna@mvc.ac.th>
2019-02-23 13:11:06 +0700

    Update App After Lunch

---
+++ app/src/main/java/mvc/ac/th/samfriend/MainActivity.java
@@ -0,0 +1,19 @@
+package mvc.ac.th.samfriend;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new BlankFragment()).commit();
+    } // Main Method
+
+} // Main Class
